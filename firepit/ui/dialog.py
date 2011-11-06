1b23f5254e153034f7f3fc4f8fb9f2f022f5e26a
Craig Slusher <cslush@gmail.com>
2011-11-05 20:56:23 -0700

    connects the preferences dialog event

--- firepit/ui/dialog.py
+++ firepit/ui/dialog.py
@@ -3,3 +3,7 @@ from firepit.ui import Dialog
 class AboutDialog(Dialog):
     def __init__(self, application):
         super(AboutDialog, self).__init__('about_dialog', application)
+
+class PreferencesDialog(Dialog):
+    def __init__(self, application):
+        super(PreferencesDialog, self).__init__('preferences_dialog', application)
--- firepit/ui/window.py
+++ firepit/ui/window.py
@@ -1,6 +1,6 @@
-from gi.repository import Gtk
 from firepit.ui import Window
-from firepit.ui.dialog import AboutDialog
+from firepit.ui.dialog import AboutDialog, PreferencesDialog
+from gi.repository import Gtk
 
 class MainWindow(Window):
     def __init__(self, application):
@@ -9,7 +9,12 @@ class MainWindow(Window):
     def connect_signals(self):
         self.connect_signal(self.widget, 'destroy', self.on_quit_event)
         self.connect_signal('quit_menu_item', 'activate', self.on_quit_event)
+        self.connect_signal('preferences_button', 'clicked', self.on_preferences_event)
+        self.connect_signal('preferences_menu_item', 'activate', self.on_preferences_event)
         self.connect_signal('about_menu_item', 'activate', self.on_about_activate)
+
+    def on_preferences_event(self, widget):
+        PreferencesDialog(self.application).show()
 
     def on_about_activate(self, widget):
         AboutDialog(self.application).show()
